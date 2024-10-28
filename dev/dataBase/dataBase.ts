6ed50f6003d969c47ae086692c4d501a45fe7880
Carduus0 <s.carduus@gmail.com>
2024-10-28 23:11:08 +0300

    feat: add types

---
+++ dev/dataBase/dataBase.ts
@@ -0,0 +1,12 @@
+export type PlayerData = {
+    name: string;
+    password: string;
+    wins: number;
+};
+
+
+export type RoomUser = {
+    id: string;
+    name: string;
+    index: number | string;
+};
